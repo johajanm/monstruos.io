cec87fafb7d988be07f71c0b5e9937033cbe903c
Johajan Martin <66478387+johajanm@users.noreply.github.com>
2023-03-12 17:07:18 -0500

    Add files via upload

---
+++ assets/js/sweetAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    html: '<h3 class="home__subtitle"> Ultimos cupos disponibles!!</h3> <p class="home__description">No dejes pasar esta gran oportunidad!!</p> <a href="https://www.financeads.net/tc.php?t=51568C336662754T&subid=Monstruos" target="_blank" class="button">Aprovecha!!</a>', 
+    background: 'rgba( 0, 0, 0, 0.9 )',
+    confirmButtonColor: '#ED1B2F',
+    icon: 'info',
+    iconColor: '#ED1B2F',
+    confirmButtonText: 'Siguiente',
+});
